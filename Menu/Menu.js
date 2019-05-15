084ec67f34e27d157a9f0109108094b54d28b04f
openwell <timileyinojo@yahoo.com>
2019-05-15 16:14:55 +0100

    added task 3

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,7 +1,8 @@
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
-  menuButton.classList.toggle("menu--open")
+ menu.classList.toggle("menu--open")
+  TweenMax.from('.menu--open', 2, {x: -200})
 }
 
 // Start Here: Create a reference to the ".menu" class
@@ -11,5 +12,15 @@ const menuButton = document.querySelector('.menu-button')
 // Using your menuButton reference, add a click handler that calls toggleMenu
 
 menuButton.addEventListener('click', ()=>{
+
   toggleMenu()
 })
+
+
+// TweenMax.from(target, duration, {vars})
+// x/y  position of 100 (same as transform: translateX(100px))
+// opacity rotation scale ease/others top/bottom or so delay/not needed for 
+// timeScale for timeline(6)
+// tweenMax needed staggerFrom(target, durationOfAnimation, {vars}, staggerAmount)
+
+ TweenMax.from('body', 5, {opacity: 0})
